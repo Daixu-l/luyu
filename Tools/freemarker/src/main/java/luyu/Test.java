7af14717e1907a2d2b5dc9f10741848caf94f507
daixu <237760330@qq.com>
2020-05-08 09:40:53 -0400

    # 对freemarker中一些常用api的使用和测试

--- Tools/freemarker/src/main/java/luyu/Test.java
+++ Tools/freemarker/src/main/java/luyu/Test.java
@@ -3,6 +3,7 @@ package luyu;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import freemarker.template.TemplateException;
+import freemarker.template.utility.DateUtil;
 
 import java.io.IOException;
 import java.util.Date;
@@ -15,8 +16,21 @@ import java.util.HashMap;
 public class Test {
 
     public static void main(String[] args) throws IOException, TemplateException {
+        System.out.println(testCombinationPrintlnString());
 
-        System.out.println(loopPrintlnString());
+    }
+
+    //测试同时使用两个api情况下的打印
+    private static String testCombinationPrintlnString() throws IOException, TemplateException {
+        HashMap<String, Object> templateDataMap = new HashMap<String, Object>();
+        JSONObject templateData = new JSONObject();
+        templateData.put("string", "    777777777777777777777777777   ");
+
+        //测试去空格的同时截取字符串长度
+        String template1 = "test string ${data.string?trim?substring(0,8)}";
+
+        templateDataMap.put("data", templateData);
+        return FreemarkerUtil.processTemplate(template1, templateDataMap);
     }
 
     //测试循环情况下的打印格式
@@ -46,7 +60,7 @@ public class Test {
     private static String testPrintlnString() throws IOException, TemplateException {
         HashMap<String, Object> templateDataMap = new HashMap<String, Object>();
         JSONObject templateData = new JSONObject();
-        templateData.put("string", " arb bucks ");
+        templateData.put("string", "777777777777777777777777777");
 
         //将字符串中的首字母大写,不会判断字符串中首字母是否为空
         String template1 = "test string ${data.string?cap_first}";
@@ -64,17 +78,19 @@ public class Test {
         String template7 = "test string ${data.string?right_pad(35,\"~~~\")}";
         //制定一个字符串的长度如果不足次长度在左侧拼接制定的字符串 默认为空格
         String template8 = "test string ${data.string?left_pad(35,\"~~~\")}";
-
+        //截取字符串指定长度从多少开始截取多少位
+        String template9 = "test string ${data.string?substring(0,8)}";
 
         templateDataMap.put("data", templateData);
-        return FreemarkerUtil.processTemplate(template8, templateDataMap);
+        return FreemarkerUtil.processTemplate(template9, templateDataMap);
     }
+
 
     //测试各种数字的打印格式
     private static String testPrintlnNumber() throws IOException, TemplateException {
         HashMap<String, Object> templateDataMap = new HashMap<String, Object>();
         JSONObject templateData = new JSONObject();
-        templateData.put("number", 999);
+        templateData.put("number", 1111);
 
         //将浮点类型的数字精确到小数点后一位且不会自动四舍五入 后面的0个数代表精确的位数以此类推,如果原来的数字位数不够则用0填充
         String template1 = "test number ${data.number?string[\"0.00\"]}}";
@@ -83,10 +99,11 @@ public class Test {
         //将数字转化为百分数进行输出
         String template3 = "test number ${data.number?string[\"percent\"]}}";
         //将数字以0000的形式输出,超过0的位数后显示真实的数字,如果遇到浮点类型的数会自动四舍五入
-        String template4 = "test number ${data.number?string[\"0000000\"]}}";
-
+        String template4 = "test number ${data.number?string[\"0000000\"]}";
+        //将数字输出去掉 ,
+        String template5 = "test number ${data.number?c}";
         templateDataMap.put("data", templateData);
-        return FreemarkerUtil.processTemplate(template4, templateDataMap);
+        return FreemarkerUtil.processTemplate(template5, templateDataMap);
     }
 
 
